69621844a8b9c5331fcd07adbf8ee766fa184fbc
mircea bardan <bardan.mircea.calin@gmail.com>
2023-06-05 14:35:57 +0200

    added isDrawCondition()

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,59 +1,87 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const cells = document.querySelectorAll(".cell")
+  const cells = document.querySelectorAll(".cell");
 
-    const players = ["X", "O"];
-    let currentPlayer = players[Math.floor(Math.random() * players.length) ]
+  const players = ["X", "O"];
+  let currentPlayer = players[Math.floor(Math.random() * players.length)];
 
-    document.getElementsByClassName("button1")[0].addEventListener("click", () => {
-        cells.forEach( (cell) => {
-            cell.textContent = "";
-            cell.removeAttribute("style", "color:red")
-        })
-    })
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      if (cell.textContent === "") {
+        cell.textContent = currentPlayer;
+      }
 
-    cells.forEach((cell) => {
-        cell.addEventListener("click", () => {
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-            if( cell.textContent === ""){
-                cell.textContent = currentPlayer
-            }
+      if (isWinningCondition(cells)) {
+        console.log(`${currentPlayer} wins`);
+      } else if (isDrawCondition(cells)) {
+        console.log("Draw");
+      }
+    });
+  });
 
-            if(isWinningCondition(cells)){
-                console.log(`winner: is ${currentPlayer}`)
-                return;
-            }
-        })
-    })
+  document
+    .getElementsByClassName("button1")[0]
+    .addEventListener("click", () => {
+      cells.forEach((cell) => {
+        cell.textContent = "";
+        cell.removeAttribute("style", "color:red");
+      });
+    });
 
-    function isWinningCondition(cells){
-        const combinations = getPossibleWinningCombinations(cells);
-   
-        for(let combination of combinations){
-            if(combination[0].textContent && combination[1].textContent && combination[2].textContent){
-                if(combination[0].textContent === combination[1].textContent && combination[1].textContent === combination[2].textContent){
-                    combination.forEach((cell) => {
-                        cell.setAttribute("style", "color:red")
-                    })
-                    return true;
-                }
-            } 
+  function isWinningCondition(cells) {
+    const combinations = getPossibleWinningCombinations(cells);
+
+    for (let combination of combinations) {
+      if (
+        combination[0].textContent &&
+        combination[1].textContent &&
+        combination[2].textContent
+      ) {
+        if (
+          combination[0].textContent === combination[1].textContent &&
+          combination[1].textContent === combination[2].textContent
+        ) {
+          combination.forEach((cell) => {
+            cell.setAttribute("style", "color:red");
+          });
+          return true;
         }
-        return false
+      }
     }
-})
+    return false;
+  }
+});
+
+function isDrawCondition(cells) {
+  for (const cell of cells) {
+    if (!cell.textContent) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function getPossibleWinningCombinations(cells) {
-    const firstRow = [cells[0], cells[1], cells[2]];
-    const secondRow = [cells[3], cells[4], cells[5]];
-    const thirdRow = [cells[6], cells[7], cells[8]];
+  const firstRow = [cells[0], cells[1], cells[2]];
+  const secondRow = [cells[3], cells[4], cells[5]];
+  const thirdRow = [cells[6], cells[7], cells[8]];
 
-    const firstColumn = [cells[0], cells[3], cells[6]];
-    const secondColumn = [cells[1], cells[4], cells[7]];
-    const thirdColumn = [cells[2], cells[5], cells[8]];
+  const firstColumn = [cells[0], cells[3], cells[6]];
+  const secondColumn = [cells[1], cells[4], cells[7]];
+  const thirdColumn = [cells[2], cells[5], cells[8]];
 
-    const firstDiagonal = [cells[0], cells[4], cells[8]];
-    const secondDiagonal = [cells[2], cells[4], cells[6]];
+  const firstDiagonal = [cells[0], cells[4], cells[8]];
+  const secondDiagonal = [cells[2], cells[4], cells[6]];
 
-    const combinations = [firstRow, secondRow, thirdRow, firstColumn, secondColumn, thirdColumn, firstDiagonal, secondDiagonal];
-    return combinations;
+  const combinations = [
+    firstRow,
+    secondRow,
+    thirdRow,
+    firstColumn,
+    secondColumn,
+    thirdColumn,
+    firstDiagonal,
+    secondDiagonal,
+  ];
+  return combinations;
 }
